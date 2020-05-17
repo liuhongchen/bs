788f33997698713c0b9ffd86aad2d531a8afbe8b
Vincent <halo_0@icloud.com>
2020-05-17 22:26:01 +0800

    pc

--- bs-sleuth-server/src/main/java/com/liuhongchen/bssleuthserver/BsSleuthServerApplication.java
+++ bs-sleuth-server/src/main/java/com/liuhongchen/bssleuthserver/BsSleuthServerApplication.java
@@ -2,11 +2,10 @@ package com.liuhongchen.bssleuthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
 public class BsSleuthServerApplication {
 
     public static void main(String[] args) {
